44c8e175b078bb9d67ca6bf0359b6b0d6b03f0e1
Rizqial5 <bagusriski1@gmail.com>
2025-02-19 10:25:13 +0700

    add: ImageModel for container ImageDTO

---
+++ Models/AlumniImageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlumniManagement.Frontend.Models
+{
+    public class AlumniImageModel
+    {
+        public int ImageID { get; set; }
+
+        public int AlumniID { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string FileName { get; set; }
+
+        public System.DateTime UploadDate { get; set; }
+    }
+}
