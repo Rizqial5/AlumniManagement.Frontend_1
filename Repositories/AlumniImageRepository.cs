dfbffae6cca8604a4dc5c377d7c2e5b0904b4e7e
Rizqial5 <bagusriski1@gmail.com>
2025-02-19 10:44:10 +0700

    add: AlumniImageRepository and IAlumniImageRepository

--- Repositories/AlumniImageRepository.cs
+++ Repositories/AlumniImageRepository.cs
@@ -1,5 +1,9 @@
 ﻿using AlumniManagement.Frontend.AlumniImageService;
+using AlumniManagement.Frontend.AlumniService;
 using AlumniManagement.Frontend.Interfaces;
+using AlumniManagement.Frontend.JobHistoryService;
+using AlumniManagement.Frontend.Models;
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +14,71 @@ namespace AlumniManagement.Frontend.Repositories
 {
     public class AlumniImageRepository : IAlumniImageRepository
     {
+        private AlumniImageServiceClient _imageServiceClient;
+        private AlumniServiceClient _alumniServiceclient;
 
+        public AlumniImageRepository()
+        {
+            _imageServiceClient = new AlumniImageServiceClient();
+            _alumniServiceclient = new AlumniServiceClient();
+        }
+
+        public async Task AddImageAsync(IEnumerable<AlumniImageModel> imageDTO, int alumniId)
+        {
+            var result = Mapping.Mapper.Map<List<ImageDTO>>(imageDTO);
+
+            await _imageServiceClient.AddImageAsync(result.ToArray(), alumniId);
+        }
+
+        public async Task DeleteImageByIdAsync(int imageId, int alumniId)
+        {
+            var alumni = _alumniServiceclient.GetAlumni(alumniId);
+            if (alumni != null)
+            {
+                var existingImage = _imageServiceClient.GetImageById(imageId,alumniId);
+                
+                await _imageServiceClient.DeleteImageByIdAsync(imageId, alumniId);
+            }
+            else
+            {
+                throw new Exception("Data not found");
+            }
+        }
+
+        public IEnumerable<AlumniImageModel> GetAllImage(int alumniId)
+        {
+            var alumni = _alumniServiceclient.GetAlumni(alumniId);
+            if (alumni != null)
+            {
+                var images = _imageServiceClient.GetAllImage( alumniId);
+
+                var result = Mapping.Mapper.Map<IEnumerable<AlumniImageModel>>(images);
+
+                return result;
+            }
+            else
+            {
+                return Enumerable.Empty<AlumniImageModel>();
+                            
+            }
+        }
+
+        public AlumniImageModel GetImageById(int imageId, int alumniId)
+        {
+            var alumni = _alumniServiceclient.GetAlumni(alumniId);
+            if (alumni != null)
+            {
+                var images = _imageServiceClient.GetImageById(imageId,alumniId);
+
+                var result = Mapping.Mapper.Map<AlumniImageModel>(images);
+
+                return result;
+            }
+            else
+            {
+                return null;
+
+            }
+        }
     }
 }
